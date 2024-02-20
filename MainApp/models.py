e0eb24004d91fef182f93f83ce35544594031e14
Roman <roma419@mail.ru>
2024-02-20 12:55:46 +0300

    Done M3_part2 practice

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
+    description = models.TextField(max_length=200, default="Temporary not filled")
 
     def __repr__(self):
         return f'Item({self.name}, {self.brand}, {self.count})'
