6804be0a8d796a542c7a5ba8a11a8973382dba9f
Roman <roma419@mail.ru>
2024-02-20 20:38:25 +0300

    Done M4_part1 practice 1, 3

---
+++ MainApp_item.sql
@@ -0,0 +1,6 @@
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (1,'Кроссовки','abibas',10,'Temporary not filled');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (2,'Куртка кожаная','Фабрика Текстиль',2,'Temporary not filled');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (3,'Кепка','Nike',124,'Temporary not filled');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (4,'Картофель фри','Точка',0,'Temporary not filled');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (5,'Газировка','Coca-Cola',12,'Temporary not filled');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (6,'Вода','Черноголовка',8,'Вода простая, вкусная');
